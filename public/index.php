1b55577b48649335c23e4cd48fee5366d9f61663
morotijani <tjhackx111@gmail.com>
2024-08-04 18:58:35 +0000

    initial commit

---
+++ public/index.php
@@ -0,0 +1,38 @@
+<?php
+	declare(strict_types=1);
+	
+	require "../bootstrap.php";
+
+	use Src\DataController;
+	use Src\Jwt;
+	use Src\Auth;
+	use Src\TableGateways\ApiUserGateway;
+    use Src\TableGateways\RefreshTokenGateway;
+
+	$requestMethod = $_SERVER["REQUEST_METHOD"];
+	$controller = new DataController($dbConnection, $requestMethod);
+ 	
+ 	if (isset($_GET['url'])) {
+		header("Access-Control-Allow-Origin: *");
+		header("Content-type: application/json; charset=UTF-8");
+		header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+		header("Access-Control-Max-Age: 3600");
+		header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+		
+		$user = new ApiUserGateway($dbConnection);
+		
+		$JwtCtrl = new Jwt($_ENV["SECRET_KEY"]);
+		$auth = new Auth($user, $JwtCtrl);
+
+		if (! $auth->authenticateJWTToken()) {
+			header("HTTP/1.1 401 Unauthorized");
+	    	exit();
+		}
+		
+		// Automatically delete expired tokens
+		$delete_expired = new RefreshTokenGateway($dbConnection, '');
+	    $delete_expired->deleteExpired();
+
+	 	$controller->result;
+ 	}
+
---
+++ public/tokens.php
@@ -0,0 +1,26 @@
+<?php
+
+	use Src\Jwt;
+	
+	// contains detailed user information
+	$payload = [
+		"sub" => $user[0]->id,
+		"name" => $user[0]->name,
+		"exp" => time() + 20 // expiration is shorter (20 seconds) for security reasons
+	];
+
+	$JwtController = new Jwt($_ENV["SECRET_KEY"]);
+
+	$access_token = $JwtController->encode($payload);
+
+	$refresh_token_expiry = time() + 432000; // expiration (5 days) to allow for longer-lasting authentication without needing frequent logins
+
+	$refresh_token = $JwtController->encode([
+		"sub" => $user[0]->id,
+		"exp" => $refresh_token_expiry
+	]);
+
+	echo json_encode([
+		"access_token" => $access_token,
+		"refresh_token" => $refresh_token
+	]);
