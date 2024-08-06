7ea064b8223644378756528ef9f17e6c59dced8c
morotijani <tjhackx111@gmail.com>
2024-08-06 04:01:10 +0000

    initial commit

--- public/home.php
+++ public/home.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="auto">
 <head>
@@ -11,8 +10,7 @@
     <meta name="generator" content="Hugo 0.122.0">
     <title>MY API · Bootstrap v0.0.1</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/jumbotrons/">
-	<link href="../assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- Favicons -->
 	<link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
@@ -424,6 +422,5 @@
 	  </footer>
 	</div>
 
-	<script src="../assets/js/bootstrap.bundle.min.js"></script>
-</body>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script></body>
 </html>
