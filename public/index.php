76df2a7a251b13710716ace98056dc7befbb9f9d
morotijani <tjhackx111@gmail.com>
2024-09-09 16:23:41 +0000

    initial commit

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 	declare(strict_types=1);
 
-	ini_set('display_errors', '1');
-	ini_set('display_startup_errors', '1');
-	error_reporting(E_ALL);
+	// ini_set('display_errors', '1');
+	// ini_set('display_startup_errors', '1');
+	// error_reporting(E_ALL);
 
 	
 	require "../bootstrap.php";
